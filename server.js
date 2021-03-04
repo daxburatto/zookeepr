dceaf06927443f584b6fb34c798f5d973c285ce1
Dax Buratto <daxburatto@gmail.com>
2021-03-03 19:22:00 -0600

    Add Heroku

--- server.js
+++ server.js
@@ -1,6 +1,42 @@
+const { query } = require('express')
 const express = require('express')
 const app = express()
+const { animals } = require('./data/animals.json')
 
+function filterByQuery(query, animalsArray) {
+    let personalityTraitsArray = []
+    let filteredResults = animalsArray
+    if (query.personalityTraits) {
+        if (typeof query.personalityTraits === 'string') {
+            personalityTraits = [query.personalityTraits]
+        } else {
+            personalityTraits = query.personalityTraits
+        }
+        personalityTraitsArray.forEach(trait => {
+            filteredResults = filteredResults.filter(
+                animal => animal.personalityTraits.indexOf(trait) !== -1
+            )
+        })
+    }
+    if (query.diet) {
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet)
+    }
+    if (query.species) {
+        filteredResults = filteredResults.filter(animal => animal.species === query.species)
+    }
+    if (query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name)
+    }
+    return  filteredResults
+}
+
+app.get('/api/animals', (req, res) => {
+    let results = animals
+    if (req.query) {
+        results = filterByQuery(req.query, results)
+    }
+    res.json(results)
+})
 app.listen(3001, () => {
     console.log(`API server now on port 3001!`)
 })
